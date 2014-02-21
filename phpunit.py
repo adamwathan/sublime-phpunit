78fffe9c65c9a44f905bb142b3bcae21d63ff86f
Adam Wathan <adam.wathan@gmail.com>
2014-02-21 01:31:35 -0500

    Add code/test switching support

--- phpunit.py
+++ phpunit.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shlex
+import ntpath
 import subprocess
 import sublime
 import sublime_plugin
@@ -33,3 +34,30 @@ class RunPhpunitTestCommand(sublime_plugin.WindowCommand):
         osascript_command += ' "PHPUnit Tests"'
         os.system(osascript_command)
         # subprocess.Popen("""osascript -e 'tell application "Sublime Text" to activate' """, shell=True)
+
+
+class FindMatchingTestCommand(sublime_plugin.WindowCommand):
+    def __init__(self, *args, **kwargs):
+        super(FindMatchingTestCommand, self).__init__(*args, **kwargs)
+
+    def path_leaf(self, path):
+        head, tail = ntpath.split(path)
+        return tail or ntpath.basename(head)
+
+    def run(self, *args, **kwargs):
+        file_name = self.window.active_view().file_name()
+        file_name = self.path_leaf(file_name)
+        file_name = file_name[0:file_name.find('.')]
+
+        if 'Test' not in file_name:
+            file_name = file_name + 'Test'
+        else:
+            # Strip 'Test' and add '.' to force matching the non-test file
+            file_name = file_name[0:file_name.find('Test')] + '.'
+
+        self.window.run_command("show_overlay", {"overlay": "goto", "text": file_name, "show_files": "true"})
+        self.window.run_command("move", {"by": "lines", "forward": False})
+
+        # This is a dirty hack to get it to switch files... Can't simulate 'Enter'
+        # but triggering the overlay again to close it seems to have the same effect.
+        self.window.run_command("show_overlay", {"overlay": "goto", "show_files": "true"})
